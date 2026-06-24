060b0d049c7442cc29a136ff4f6a828091ce6675
samu <samuele.longo@studenti.unimi.it>
2026-06-24 12:05:05 +0200

    VERDE DIRETTO: testGetDescription, la corretta descrizione dell'estensione

--- src/test/java/qupath/ext/template/AnnotationExtractorExporterTest.java
+++ src/test/java/qupath/ext/template/AnnotationExtractorExporterTest.java
@@ -18,4 +18,9 @@ class AnnotationExtractorExporterTest {
     void testGetName() {
         assertEquals("Manual Annotation Exporter", SUT.getName());
     }
+
+    @Test
+    void testGetDescription() {
+        assertEquals("This extension allows you to export manual annotations from images of the current project.", SUT.getDescription());
+    }
 }
