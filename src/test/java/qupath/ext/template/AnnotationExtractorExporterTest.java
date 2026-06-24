b03f8a8021269c2df3fad820087f41c6d1e58551
samu <samuele.longo@studenti.unimi.it>
2026-06-24 12:08:04 +0200

    VERDE DIRETTO: testQupathVersion, la corretta versione dell'estensione

--- src/test/java/qupath/ext/template/AnnotationExtractorExporterTest.java
+++ src/test/java/qupath/ext/template/AnnotationExtractorExporterTest.java
@@ -23,4 +23,9 @@ class AnnotationExtractorExporterTest {
     void testGetDescription() {
         assertEquals("This extension allows you to export manual annotations from images of the current project.", SUT.getDescription());
     }
+
+    @Test
+    void testQupathVersion() {
+        assertEquals("0.7.0", SUT.getQuPathVersion().toString());
+    }
 }
